06876f75e27124cbab8800a0cd4bebcb1b9af852
BCITKevin <kso51@my.bcit.ca>
2024-04-04 14:35:23 -0700

    first commit

---
+++ databaseConnection.js
@@ -0,0 +1,12 @@
+const database = require("mongoose");
+const is_qoddi = process.env.IS_QODDI || false;
+const databaseName = "lab_example"
+// const qoddiURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
+const qoddiURI = "mongodb+srv://root:kevin02gusgh02@cluster0.yz1megk.mongodb.net/"
+const localURI = "mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
+if (is_qoddi) {
+database.connect(qoddiURI, {useNewUrlParser: true, useUnifiedTopology: true});
+}
+else {
+database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+}
